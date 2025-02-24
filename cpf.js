5626f8d0e7a100cb0341dbdba56d0090d26b8f28
cauantrapp@outlook.com <caua_trapp@estudante.sesisenai.org.br>
2025-02-24 11:19:30 -0300

    add verificar cpf

--- cpf.js
+++ cpf.js
@@ -1,22 +1,46 @@
-let cpf = '090.851.019-56'
+function calculoLoop(contador, cpfsodigitos) {
+    let soma = 0;
 
-function verificarCPF (cpf){
-    let cpfsodigitos = cpf
-    .replaceAll('.', '')
-    .replaceAll('-', '')
-    .split('')
-    if(cpfsodigitos.lenght != 11){
-        return 'CPF inválido'
+    for (let i = contador; i > 1; i--) {
+        soma += cpfsodigitos[contador - i] * i;
     }
 
-    let soma = 0
-
-    for(i=10; i>1; i--){
-        soma += cpfsodigitos[10-i] * i
-
+    let resultado = soma % 11;
+    if (resultado < 2) {
+        return cpfsodigitos[contador - 1] == 0;
+    } else {
+        return cpfsodigitos[contador - 1] == (11 - resultado);
     }
-    return soma
-
-
 }
-console.log(verificarCPF(cpf))
+
+function verificarCPF(cpf) {
+    let cpfsodigitos = cpf
+        .replaceAll('.', '')
+        .replaceAll('-', '')
+        .split('');
+
+
+    if (cpfsodigitos.length !== 11) {
+        return 'CPF inválido';
+    }
+    
+   
+    let resposta = calculoLoop(10, cpfsodigitos);
+    if (!resposta) {
+        return 'CPF inválido';
+    }
+
+  
+    let resposta2 = calculoLoop(11, cpfsodigitos);
+    if (!resposta2) {
+        return 'CPF inválido';
+    }
+
+    return 'CPF válido';
+}
+
+let cpf = '090.851.019-56';
+let cpf2 = '090.851.019-16';
+
+console.log(verificarCPF(cpf));  
+console.log(verificarCPF(cpf2)); 
